fb736ba883e1a592d3118ccf3afaf81d42359c67
Gas1mzade <qasimovnihat3@gmail.com>
2023-05-18 14:57:05 +0000

    .

--- app.js
+++ app.js
@@ -1,4 +1,55 @@
 "use strict";
 $(document).ready(function () {
 
+    
+
+$('.btn').click(function (e) { 
+    e.preventDefault();
+    let a = a => {
+        if (Number(a)){
+            for (let i = 0; i <= a; i++) {
+            $('.box').append(`<h1>${a}</h1>`);
+            
+        }
+        }else{
+            $('.box').append(`<h1>${a}</h1>`);
+        }
+    }
+    a($('input').val())
+});
+
+
+
+// $('.btn').click(function (e) {
+//     e.preventDefault();
+//     let a = (a) => {
+//         if (Number(a)) {
+//             for (let i = 0; i < a; i++) {
+//                 $('.box').append(`<h2>${a}</h2>`);
+//             }
+//         } else {
+//             if (String(a)) {
+//                 $('.box').append(`<h2>${a}</h2>`);
+//             }
+//         }
+//     }
+//     a($('input').val());
+//     $('input').val("");
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
